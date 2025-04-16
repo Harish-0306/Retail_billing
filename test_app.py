9e53425286fce723fc2d0c0a610cdbabfbc20929
Rajeshkanna <mrajes107@apac.comcast.com>
2025-04-16 17:46:54 +0530

    first commit

--- test_app.py
+++ test_app.py
@@ -1,11 +1,19 @@
 import unittest
 from app import app
 
-class FlaskTestCase(unittest.TestCase):
-    def test_home_redirect(self):
-        tester = app.test_client(self)
-        response = tester.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
 
-if __name__ == '__main__':
+    def test_home_redirects_to_login(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/login', response.location)
+
+    def test_login_page_loads(self):
+        response = self.app.get('/login')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'LOGIN', response.data)
+
+if __name__ == "__main__":
     unittest.main()
