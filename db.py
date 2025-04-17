af244de5736a1e0913d9dfb09e7e9ecbacf34b23
Rajeshkanna <mrajes107@apac.comcast.com>
2025-04-17 16:44:10 +0530

    Last commit6

--- db.py
+++ db.py
@@ -2,9 +2,10 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="host.docker.internal",  # Docker → Host connection
+        host="172.31.35.156",  # or host.docker.internal
         user="root",
         password="Muthupattan@1403",
         database="retail_store",
         cursorclass=pymysql.cursors.DictCursor
     )
+
