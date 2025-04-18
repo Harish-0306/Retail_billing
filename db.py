587c1f7dc764ba10e1d3b2c623aba1d87a4ee5f1
Rajeshkanna <mrajes107@apac.comcast.com>
2025-04-18 13:04:46 +0530

    Last commit134

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="172.31.35.156",  # or host.docker.internal
+        host="172.31.45.106",  # or host.docker.internal
         user="root",
         password="Muthupattan@1403",
         database="retail_store",
